20f0e01d348c47c4dfa5248dc9a6fc7d6bb8c698
Mark Jasper Rojas <markjasperrojas1@gmail.com>
2026-06-28 15:01:54 +0800

    Setup webpack and implement webpack merge

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,41 +1,11 @@
-import path from "node:path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import { merge } from "webpack-merge";
+import common from "./webpack.common.js";
 
-export default {
+export default merge(common, {
   mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(import.meta.dirname, "dist"),
-    clean: true,
-  },
-  devtool: "eval-source-map",
+  devtool: "inline-source-map",
   devServer: {
+    static: "./dist",
     watchFiles: ["./src/template.html"],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.html$/i,
-        use: ["html-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(mp3|wav|ogg)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
-};
+});
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,41 +1,6 @@
-import path from "node:path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import { merge } from "webpack-merge";
+import common from "./webpack.common.js";
 
-export default {
+export default merge(common, {
   mode: "production",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(import.meta.dirname, "dist"),
-    clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.html$/i,
-        use: ["html-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(mp3|wav|ogg)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
-};
+});
